5ddfef620166a2ee466fbc8e03eea68325d106ec
Azizbek Asadov <azizbek1998@icloud.com>
2022-11-02 01:00:24 +0500

    Initial Commit

---
+++ Bull's Eye/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bull's Eye
+//
+//  Created by Azizbek Asadov on 02/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
