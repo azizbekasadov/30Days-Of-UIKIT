bcc25793ff9ea80e31daf615440e8b6738720c2b
Azizbek Asadov <azizbek1998@icloud.com>
2022-11-02 02:06:00 +0500

    Basic setup of the app

--- Bull's Eye/ViewController.swift
+++ Bull's Eye/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hitMeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: "This is my first app!",
+            preferredStyle: .alert)
+
+          let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+
+          alert.addAction(action)
+          present(alert, animated: true, completion: nil)
+    }
 
 }
 
