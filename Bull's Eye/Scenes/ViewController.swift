e5ae0c87a2dd2a334246aac52b077b4040b8570b
Azizbek Asadov <azizbek1998@icloud.com>
2022-11-03 00:58:04 +0500

    Setup of the basic template of the future UI

--- Bull's Eye/Scenes/ViewController.swift
+++ Bull's Eye/Scenes/ViewController.swift
@@ -8,28 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var hitMeButton: UIButton!
+    
+    var currentValue: Int = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func showAlert(_ sender: UIButton) {
+        let message = "The value of the slider is: \(currentValue)"
         let alert = UIAlertController(
             title: "Hello, World",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert)
-
-          let action = UIAlertAction(
+        
+        let action = UIAlertAction(
             title: "Awesome",
             style: .default,
             handler: nil)
-
-          alert.addAction(action)
-          present(alert, animated: true, completion: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
 }
-
